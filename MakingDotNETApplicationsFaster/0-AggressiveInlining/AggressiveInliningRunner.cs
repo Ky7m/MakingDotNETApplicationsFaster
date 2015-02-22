f4c9e701a6f7cfaef857d07d2c1e86ca167d0b67
Igor Fesenko <igor.aka.ky7m@gmail.com>
2015-02-22 13:04:42 +0200

    Added a new method with additional attributes connected to  AggressiveInlining feature.

--- MakingDotNETApplicationsFaster/0-AggressiveInlining/AggressiveInliningRunner.cs
+++ MakingDotNETApplicationsFaster/0-AggressiveInlining/AggressiveInliningRunner.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.CompilerServices;
+using System.Security;
 using MakingDotNETApplicationsFaster.Infrastructure;
 
 namespace MakingDotNETApplicationsFaster
@@ -11,7 +13,8 @@ namespace MakingDotNETApplicationsFaster
             {
                 {i => { SmallMethod(i, i + 3); }, "SmallMethod"},
                 {i => { NoInliningLargeMethod(i, i + 3); }, "NoInliningLargeMethod"},
-                {i => { InliningLargeMethod(i, i + 3); }, "InliningLargeMethod"}
+                {i => { InliningLargeMethod(i, i + 3); }, "InliningLargeMethod"},
+                {i => { LargeMethodWithAdditionalAttributes(i, i + 3); }, "LargeMethodWithAdditionalAttributes"}
             }.Run(10000000);
         }
 
@@ -37,6 +40,25 @@ namespace MakingDotNETApplicationsFaster
 
         //[MethodImpl(MethodImplOptions.NoInlining)]
         private static int NoInliningLargeMethod(int i, int j)
+        {
+            if (i + 14 > j)
+            {
+                return i + j;
+            }
+            if (j*12 < i)
+            {
+                return 42 + i - j*7;
+            }
+            return i%14 - j;
+        }
+		
+		// The attributes on this method are chosen for the best JIT performance. 
+		// Based on a following finding https://github.com/dotnet/coreclr/blob/cbf46fb0b6a0b209ed1caf4a680910b383e68cba/src/mscorlib/src/System/Buffer.cs#L536
+		[SuppressUnmanagedCodeSecurity]
+		[SecurityCritical]
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[CLSCompliant(false)]
+		private static int LargeMethodWithAdditionalAttributes(int i, int j)
         {
             if (i + 14 > j)
             {

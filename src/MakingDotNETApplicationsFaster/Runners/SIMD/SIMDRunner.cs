02814f4d87716a2854cf3655bb90af3e8bfdb072
Igor Fesenko <ifesen@softserveinc.com>
2015-12-14 18:35:14 +0200

    Update SIMD runner

--- src/MakingDotNETApplicationsFaster/Runners/SIMD/SIMDRunner.cs
+++ src/MakingDotNETApplicationsFaster/Runners/SIMD/SIMDRunner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics;
 using MakingDotNETApplicationsFaster.Infrastructure;
 
@@ -7,25 +8,45 @@ namespace MakingDotNETApplicationsFaster.Runners.SIMD
     {
         public void Run()
         {
-            var customVector1 = new CustomVector(1, 2, 3, 0);
-            var customVector2 = new CustomVector(3, 2, 1, 0);
+            var rand = new Random();
+            const int size = 1024 * 1024;
 
-            var bclVector1 = new Vector4(1, 2, 3, 0);
-            var bclVector2 = new Vector4(3, 2, 1, 0);
+            var a = new double[size];
+            var b = new double[size];
+            for (var i = 0; i < size; ++i)
+            {
+                a[i] = rand.NextDouble();
+                b[i] = rand.NextDouble();
+            }
+
             new PerformanceTests
             {
-                {_ => CustomVectorMultiply(customVector1, customVector2), "CustomVectorMultiply"},
-                {_ => BclVectorMultiply(bclVector1, bclVector2), "BclVectorMultiply"}
-            }.Run(100000000);
+                {_ => NativeImplementation(a, b), "NativeImplementation"},
+                {_ => NumericsVectorImplementation(a, b), "MultiplyVector"}
+            }.Run(1000);
         }
 
-        static CustomVector CustomVectorMultiply(CustomVector customVector1, CustomVector customVector2)
+        static double NativeImplementation(double[] a, double[] b)
         {
-            return customVector1 * customVector2;
+            var dp = 0.0;
+            for (var i = 0; i < a.Length; ++i)
+            {
+                dp += a[i] + b[i];
+            }
+            return dp;
         }
-        static Vector4 BclVectorMultiply(Vector4 bclVector1, Vector4 bclVector2)
+
+        static double NumericsVectorImplementation(double[] a, double[] b)
         {
-            return bclVector1 * bclVector2;
+            var dp = 0.0;
+            var vectorSize = Vector<float>.Count;
+            for (var i = 0; i < a.Length; i += vectorSize)
+            {
+                var va = new Vector<double>(a, i);
+                var vb = new Vector<double>(b, i);
+                dp += Vector.Dot(va, vb);
+            }
+            return dp;
         }
     }
 }

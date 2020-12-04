d71a7ea5165baf9c2fd3fedd58ec870f6b1e7ae5
NFlameInvasion <antonbegun30@gmail.com>
2020-12-05 00:52:43 +0300

    Add SetStructVsRefSetStruct

--- src/MakingDotNETApplicationsFaster/Program.cs
+++ src/MakingDotNETApplicationsFaster/Program.cs
@@ -7,7 +7,7 @@ namespace MakingDotNETApplicationsFaster
     {
         public static void Main()
         {
-            BenchmarkRunner.Run<ReflectionMethodInvokeVsInterfaceRunner>();
+            BenchmarkRunner.Run<SetStructVsRefSetStruct>();
         }
     }
 }
